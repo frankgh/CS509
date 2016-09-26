3b343b9e5b707371d5a8fe8fd0539668b9a272c0
Francisco Guerrero <frank.guerrero@gmail.com>
2016-09-26 18:11:41 -0400

    Added annotations for persistence

--- wordsweeper-rest/src/main/java/com/wordsweeper/service/model/Letter.java
+++ wordsweeper-rest/src/main/java/com/wordsweeper/service/model/Letter.java
@@ -1,10 +1,24 @@
 package com.wordsweeper.service.model;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
 /**
  * Created by francisco on 9/13/16.
  */
+@Entity
+@Table(name = "letter")
 public class Letter {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "id")
+    int id; /* The id of the board */
+
+    @Column(name = "character")
+    @NotNull
     char character;
+    @Column(name = "score")
     int score;
 
     Letter(char character, int score) {
