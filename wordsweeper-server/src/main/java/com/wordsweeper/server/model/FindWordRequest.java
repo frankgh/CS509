5e95490cfc855303defff888a5613186fc2f8c99
frank.guerrero <frank.guerrero@gmail.com>
2016-10-02 11:04:15 -0400

    Add model classes from xsd

---
+++ wordsweeper-server/src/main/java/com/wordsweeper/server/model/FindWordRequest.java
@@ -0,0 +1,153 @@
+
+package com.wordsweeper.server.model;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for anonymous complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType>
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element ref="{}cell" maxOccurs="unbounded"/>
+ *       &lt;/sequence>
+ *       &lt;attribute name="name" use="required" type="{http://www.w3.org/2001/XMLSchema}string" />
+ *       &lt;attribute name="word" use="required" type="{http://www.w3.org/2001/XMLSchema}string" />
+ *       &lt;attribute name="gameId" use="required" type="{http://www.w3.org/2001/XMLSchema}string" />
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "", propOrder = {
+    "cell"
+})
+@XmlRootElement(name = "findWordRequest")
+public class FindWordRequest {
+
+    @XmlElement(required = true)
+    protected List<Cell> cell;
+    @XmlAttribute(name = "name", required = true)
+    protected String name;
+    @XmlAttribute(name = "word", required = true)
+    protected String word;
+    @XmlAttribute(name = "gameId", required = true)
+    protected String gameId;
+
+    /**
+     * Gets the value of the cell property.
+     * 
+     * <p>
+     * This accessor method returns a reference to the live list,
+     * not a snapshot. Therefore any modification you make to the
+     * returned list will be present inside the JAXB object.
+     * This is why there is not a <CODE>set</CODE> method for the cell property.
+     * 
+     * <p>
+     * For example, to add a new item, do as follows:
+     * <pre>
+     *    getCell().add(newItem);
+     * </pre>
+     * 
+     * 
+     * <p>
+     * Objects of the following type(s) are allowed in the list
+     * {@link Cell }
+     * 
+     * 
+     */
+    public List<Cell> getCell() {
+        if (cell == null) {
+            cell = new ArrayList<Cell>();
+        }
+        return this.cell;
+    }
+
+    /**
+     * Gets the value of the name property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Sets the value of the name property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setName(String value) {
+        this.name = value;
+    }
+
+    /**
+     * Gets the value of the word property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getWord() {
+        return word;
+    }
+
+    /**
+     * Sets the value of the word property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setWord(String value) {
+        this.word = value;
+    }
+
+    /**
+     * Gets the value of the gameId property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getGameId() {
+        return gameId;
+    }
+
+    /**
+     * Sets the value of the gameId property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setGameId(String value) {
+        this.gameId = value;
+    }
+
+}
