9dd202cf9b79cca5be79d28b345c24a48b7e0b97
frank.guerrero <frank.guerrero@gmail.com>
2016-12-09 11:18:30 -0500

    Merge UtilTest

--- wordsweeper-rest/src/main/test/com/wordsweeper/service/util/UtilTest.java
+++ wordsweeper-rest/src/main/test/com/wordsweeper/service/util/UtilTest.java
@@ -1,29 +1,28 @@
 package com.wordsweeper.service.util;
 
-import static org.junit.Assert.*;
+import com.wordsweeper.service.model.Location;
+import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import org.junit.Test;
-
-import com.wordsweeper.service.model.Location;
+import static org.junit.Assert.*;
 
 public class UtilTest {
-	
-	@Test
-	public void constructor() throws Exception {
-		Util util = new Util();
-		List<Location> locationList = new ArrayList<>();
-		locationList = util.parseCellPositions("2,3");
-		assertEquals(locationList.size(), 1);
-		
-		locationList = util.parseCellPositions("2,3|3,4");
-		assertEquals(locationList.size(), 2);
-		
-		locationList = util.parseCellPositions("2,2,3");
-		assertEquals(locationList, new ArrayList<>());
-	}
+
+    @Test
+    public void constructor() throws Exception {
+        Util util = new Util();
+        List<Location> locationList = new ArrayList<>();
+        locationList = util.parseCellPositions("2,3");
+        assertEquals(locationList.size(), 1);
+
+        locationList = util.parseCellPositions("2,3|3,4");
+        assertEquals(locationList.size(), 2);
+
+        locationList = util.parseCellPositions("2,2,3");
+        assertEquals(locationList, new ArrayList<>());
+    }
 
     @Test
     public void testParseCellPositions1() {
@@ -71,7 +70,5 @@ public class UtilTest {
         assertNull(val);
 
     }
-	
-	
 
 }
